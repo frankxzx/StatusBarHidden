1e56a1aefae3a5079bd87876fbe56ef3f7d280a7
frank.xu <frank.xu@touch-spring.com>
2016-12-24 21:58:51 +0800

    1) add progress bar

--- StateBarHidden/ViewController.swift
+++ StateBarHidden/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import YLProgressBar
 
 class ViewController: UIViewController {
-
+    
+    struct Screen {
+        static var width = UIScreen.main.bounds.width
+        static var height = UIScreen.main.bounds.height
+    }
+    
+    var progressBar: YLProgressBar =  {
+        let progressBar = YLProgressBar(frame: CGRect(x :0, y: 0, width: Screen.width, height: 10))
+        progressBar.type = .flat
+        progressBar.isHidden = true
+        progressBar.progressTintColor = UIColor.yellow
+        progressBar.indicatorTextDisplayMode = .progress
+        progressBar.behavior = .indeterminate
+        progressBar.stripesOrientation = .vertical
+        progressBar.setProgress(0.2, animated: true)
+        return progressBar
+    }()
+    
+    var timer: DispatchSourceTimer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UIApplication.shared.keyWindow?.addSubview(progressBar)
+        
+        let queue = DispatchQueue(label: "com.touchspring.timer", attributes: .concurrent)
+        timer = DispatchSource.makeTimerSource(queue: queue)
+        timer.scheduleRepeating(deadline: .now(), interval: .seconds(1), leeway: .seconds(1))
+        timer.setEventHandler { [weak self] in
+            self?.progressAnimation()
+        }
+        
+        timer.resume()
     }
-
+    
+    let values = [0.4, 0.5, 0.7, 1.0]
+    func progressAnimation() {
+        let diceRoll = Int(arc4random_uniform(4))
+        DispatchQueue.main.async {
+            self.progressBar.setProgress(CGFloat(self.values[diceRoll]), animated: true)
+        }
+        
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setNeedsStatusBarAppearanceUpdate()
@@ -28,15 +67,17 @@ class ViewController: UIViewController {
     @IBAction func toggleStateBar(_ sender: UIButton) {
         isStatusBarHidden = !isStatusBarHidden
         setNeedsStatusBarAppearanceUpdate()
+        
+        progressBar.isHidden = !isStatusBarHidden
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return isStatusBarHidden
     }
 }
 
 class ViewControllerB: UIViewController {
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setNeedsStatusBarAppearanceUpdate()
@@ -54,7 +95,7 @@ class ViewControllerB: UIViewController {
 }
 
 extension UINavigationController {
-
+    
     open override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
         return .slide
     }
@@ -64,7 +105,7 @@ extension UINavigationController {
     }
     
     open override var childViewControllerForStatusBarHidden: UIViewController? {
-         return visibleViewController
+        return visibleViewController
     }
     
 }
