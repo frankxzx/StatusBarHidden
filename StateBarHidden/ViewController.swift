a4675ca57b2d6c384cec5034f57447647cf1adac
frank.xu <frank.xu@touch-spring.com>
2016-12-24 17:08:50 +0800

    init

--- StateBarHidden/ViewController.swift
+++ StateBarHidden/ViewController.swift
@@ -12,14 +12,59 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+    
+    var isStatusBarHidden = false
+    @IBAction func toggleStateBar(_ sender: UIButton) {
+        isStatusBarHidden = !isStatusBarHidden
+        setNeedsStatusBarAppearanceUpdate()
     }
 
-
+    override var prefersStatusBarHidden: Bool {
+        return isStatusBarHidden
+    }
 }
 
+class ViewControllerB: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
+
+extension UINavigationController {
+
+    open override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return .slide
+    }
+    
+    open override var childViewControllerForStatusBarStyle: UIViewController? {
+        return visibleViewController
+    }
+    
+    open override var childViewControllerForStatusBarHidden: UIViewController? {
+         return visibleViewController
+    }
+    
+}
